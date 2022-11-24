ed7f78bd9a9b0ef0d9b209a20d0d52c0556163ef
James <james1projects@gmail.com>
2022-11-24 13:08:37 -0700

    updating config

--- SmiteAPIConfig.py
+++ SmiteAPIConfig.py
@@ -12,146 +12,109 @@ if getattr(sys, 'frozen', False):
     myDir = os.path.dirname(sys.executable)
 elif __file__:
     myDir = os.path.dirname(__file__)
-PATH_CONFIGFILE = os.path.join(myDir, "SmiteAPIConfig.ini")
+PATH_CONFIG_FILE = os.path.join(myDir, "config.ini")
 
-class SmiteAPIConfig(object):
-    path_configFile = PATH_CONFIGFILE
-    
-    #Sections
-    #API SETTINGS
-    SECTION_API_SETTINGS = "API SETTINGS"
-    OPTION_DEV_ID = "DEV_ID"
-    OPTION_AUTH_KEY = "AUTH_KEY"
-    OPTION_PC_URL = "PC_URL"
-    OPTION_XBOX_URL = "XBOX_URL"
-    OPTION_PLAYSTATION_URL = "PLAYSTATION_URL"
-    
-    #Session. Stores current session details so a new one doesn't need to be created everytime a request is called
+class Option():
+    def __init__(self, section_name, option_name):
+        self.section_name = section_name
+        self.option_name = option_name
+        self.value = value
+
+        return self
+
+class Config():
+    path_config_file = PATH_CONFIG_FILE
+
+    # Sections
+    # API SETTINGS
+    SECTION_SMITE_API_SETTINGS = "API SETTINGS"
+    SMITE_DEV_ID = "DEV_ID"
+    SMITE_AUTH_KEY = "AUTH_KEY"
+    PC_URL = "PC_URL"
+    XBOX_URL = "XBOX_URL"
+    PLAYSTATION_URL = "PLAYSTATION_URL"
+
+    # Session. Stores current session details so a new one doesn't need to be created everytime a request is called
     SECTION_SESSION = "SESSION"
     OPTION_SESSION_ID = "SESSION_ID"
     OPTION_SESSION_TIMESTAMP = "SESSION_TIMESTAMP"
-    
-    
+
+    BLANK_STRING = " "
+
     def __init__(self):
-        #Sets the path of the config file, and the config object from import configparser
-        self.config = configparser.ConfigParser()
-        if (not os.path.exists(self.path_configFile)):
-            print(self.path_configFile, "does not exist, creating a new one which will need to be updated..")
-            self.CreateNewConfigFile()
-    
-    #Create a new SmiteAPIConfig.ini
-    def CreateNewConfigFile(self):
-        self.saveDevId("{DEV ID}")
-        self.saveAuthKey("{AUTH KEY}")
-        self.savePcUrl("https://api.smitegame.com/smiteapi.svc")
-        self.saveXboxUrl("https://api.xbox.smitegame.com/smiteapi.svc")
-        self.savePlayStationUrl("https://api.ps4.smitegame.com/smiteapi.svc")
+        # Sets the path of the config file, and the config object from import configparser
+        self.config = configparser.Config()
+        if (not os.path.exists(self.path_config_file)):
+            print("\nConfig File:", self.path_config_file, "does not exist, creating a new one.")
+            self.create_config_file()
 
-        self.WriteToConfigFile()
+    def create_config_file(self):
+        # self.save_dev_id("{DEV ID}")
+        # self.save_auth_key("{AUTH KEY}")
+        self.save_pc_url("https://api.smitegame.com/smiteapi.svc")
+        self.save_xbox_url("https://api.xbox.smitegame.com/smiteapi.svc")
+        self.save_playstation_url("https://api.ps4.smitegame.com/smiteapi.svc")
 
-           
-    def WriteToConfigFile(self):
-        #Whether the function was successful to writing to the config file or not.
+        self.save_config_file()
+
+
+    def save_config_file(self):
+        # Whether the function was successful to writing to the config file or not.
         result = False
         try:
-            with open(PATH_CONFIGFILE, 'w') as configFile:
-                self.config.write(configFile)
+            with open(self.path_config_file, 'w') as config_file:
+                self.config.write(config_file)
             result = True
         except Exception as e:
-            print("Error when writing to ", PATH_CONFIGFILE)
+            print("Unable to write to ", self.path_config_file)
             print(e)
             result = False
         return result
-    
-    #Used for reading an option from the config file
-    #section is the section name in the ini file
-    #option is the option name in the ini file
-    def loadOption(self, section, option):
+
+    def load_option(self, section_name, option_name):
+        # None values are stored as " ".
         value = None
-        self.config.read(self.path_configFile)
-        
-        if (self.config.has_section(section)):
-            #The section exists..
-            if (self.config.has_option(section, option)):
-                value = self.config[section][option]
+        self.config.read(self.path_config_file)
+        if (self.config.has_section(section_name)):
+            if (self.config.has_option(section_name, option_name)):
+                value = self.config[section_name][option_name]
             else:
-                print("No option named \"", option, "\" inside of section \"", section, "\" within \"", self.path_configFile, "\"."
+                print("No option named \"", option_name, "\" inside of section \"", section_name, "\" within \"", self.path_config_file, "\"."
                       "\n\tCheck ini file.", sep="")
         else:
-            print("No section named \"", section, "\" within \"", self.path_configFile, "\".",
+            print("No section named \"", section_name, "\" within \"", self.path_config_file, "\".",
                   "\n\tCheck ini file.", sep="")
 
-        if value == "":
+        if value == self.BLANK_STRING:
             value = None
-            
         return value
-    
-    def saveOption(self, section, option, value):
-        if (value == None):
-            value = ""
+
+    def save_option(self, section_name, option_name, value):
+        if value is None:
+            value = " "
         else:
             value = str(value)
-        
-        self.config.read(self.path_configFile)
-        if (self.config.has_section(section)):
-            self.config[section][option] = value
-        else:
-            self.config[section] = {option : value}
-            
-        self.WriteToConfigFile()
-        
-    def loadDevId(self):
-        return self.loadOption(self.SECTION_API_SETTINGS, self.OPTION_DEV_ID)
-    
-    def saveDevId(self, devId):
-        self.saveOption(self.SECTION_API_SETTINGS, self.OPTION_DEV_ID, devId)
-        
-    def loadAuthKey(self):
-        return self.loadOption(self.SECTION_API_SETTINGS, self.OPTION_AUTH_KEY)
-        
-        
-    def saveAuthKey(self, authKey):
-        self.saveOption(self.SECTION_API_SETTINGS, self.OPTION_AUTH_KEY, authKey)
-        
-    def loadPcUrl(self):
-        return self.loadOption(self.SECTION_API_SETTINGS, self.OPTION_PC_URL)
-    
-    def savePcUrl(self, pcUrl):
-        self.saveOption(self.SECTION_API_SETTINGS, self.OPTION_PC_URL, pcUrl)
-    
-    def loadXboxUrl(self):
-        return self.loadOption(self.SECTION_API_SETTINGS, self.OPTION_XBOX_URL)
-    
-    def saveXboxUrl(self, xboxUrl):
-        self.saveOption(self.SECTION_API_SETTINGS, self.OPTION_XBOX_URL, xboxUrl)
-    
-    def loadPlayStationUrl(self):
-        return self.loadOption(self.SECTION_API_SETTINGS, self.OPTION_PLAYSTATION_URL)
-    
-    def savePlayStationUrl(self, playStationUrl):
-        self.saveOption(self.SECTION_API_SETTINGS, self.OPTION_PLAYSTATION_URL, playStationUrl)
 
-    def loadSessionId(self):
-        return self.loadOption(self.SECTION_SESSION, self.OPTION_SESSION_ID)
-        
-    def saveSessionId(self, sessionId):
-        self.saveOption(self.SECTION_SESSION, self.OPTION_SESSION_ID, sessionId)
-    
-    def loadSessionTimeStamp(self):
-        return self.loadOption(self.SECTION_SESSION, self.OPTION_SESSION_TIMESTAMP)
-    
-    def saveSessionTimeStamp(self, sessionTimeStamp):
-        self.saveOption(self.SECTION_SESSION, self.OPTION_SESSION_TIMESTAMP, sessionTimeStamp)
-    
-    
-if __name__ == "__main__":
-    print("Running SmiteAPIConfig.py as main method...")
-    smiteConfig = SmiteAPIConfig()
-    #ONLY FOR HATMASTER
-    smiteConfig.saveDevId(3351)
-    smiteConfig.saveAuthKey("19BDEE17CE094121A8EFE358658864B1")
-    print(smiteConfig.loadAuthKey())
-    #smiteConfig.setDevId(500)
-    #print(smiteConfig.getDevId())
-    
-    
+        self.config.read(self.path_config_file)
+        if (self.config.has_section(section_name)):
+            self.config[section_name][option_name] = value
+        else:
+            self.config[section_name] = {option_name: value}
+
+        self.save_config_file()
+
+        return value
+
+    def load_smite_api_option(self, option_name):
+        value = None
+        section_name = self.SECTION_SMITE_API_SETTINGS
+        value = self.load_option(self, section_name, option_name)
+
+        return value
+
+    def save_smite_api_option(self, option_name, value):
+        section_name = self.SECTION_SMITE_API_SETTINGS
+        value = self.save_option(self, self.SECTION_SMITE_API_SETTINGS, option_name, value)
+
+        return value
+
